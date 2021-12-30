d05a0f790dcef1224649ac0effff281d15898024
yaboocn <yaboocn@qq.com>
2021-12-30 17:26:41 +0800

    调整快捷命令脚本

--- reload.cmd
+++ reload.cmd
@@ -1,4 +1,4 @@
-rd/s/q node_modules/fansion-base
-rd/s/q node_modules/fansion-fac
+rd/s/q node_modules\fansion-base
+rd/s/q node_modules\fansion-fac
 set NODE_OPTIONS=--openssl-legacy-provider
 npm cache clean --force&&npm install
